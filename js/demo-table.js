234a9ead3c35bff3ea626c00b4b274a0357faa50
Steven Sierra Forero <bizoru@gmail.com>
2016-07-21 07:30:16 -0500

    Agregado tablas ng-table con filtros,ordenamiento y paginacion

---
+++ js/demo-table.js
@@ -0,0 +1,38 @@
+/* http://www.mocky.io/v2/5790a35926000085167ee533
+   URL generada con datos aleatorios
+*/
+
+var app = angular.module("myapp", ["ngTable"]);
+
+app.controller("tableController",tableController);
+
+tableController.$inject = ["NgTableParams","$http"]
+function tableController(NgTableParams,$http){
+  var self = this;
+  var url = "http://www.mocky.io/v2/5790a35926000085167ee533";
+  self.tableParams = new NgTableParams({
+    count: 5 // initial page size
+  });
+
+  self.tableParams.settings({
+    counts: [5,10,20] // initial page size
+  });
+
+  self.cols = [
+    { field: "age", title: "Edad", filter: { age: "number"}, sortable: "age", show: true},
+    { field: "company", title: "Empresa", filter: { company: "text"}, sortable: "company",show: true},
+    { field: "email", title: "Correo", filter: { email: "text"}, sortable:"email",show: true}
+  ];
+
+  $http.get(url).then(function(response){
+    console.log(response.data);
+    self.tableParams.settings(
+      {
+        dataset: response.data
+      }
+
+    );
+  });
+
+
+}
